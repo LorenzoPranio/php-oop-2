88067319df98fd2b4b11b5bb4a2fcf1331915082
LorenzoPranio <lorenzopranio02@gmail.com>
2024-09-18 17:27:04 +0200

    [UPD]Added images in $products and added more products

--- Index.php
+++ Index.php
@@ -18,6 +18,7 @@ class ItemType {
     const Food = 'Cibo';
     const Toy = 'Gioco';
     const Kennel = 'Cuccia';
+    const WellBeing = 'Benessere';
 }
 
 class Product{
@@ -41,9 +42,20 @@ class Product{
 }
 
 //istanza di Product
-    $products = [
-        new Product('Crocchette per Cani', 20.99, '', $categoryDogs, ItemType::Food),
-    ];
+$products = [
+    new Product('Crocchette per Cani', 20.99, './img/crocchette-per-cani.jpeg', $categoryDogs, ItemType::Food),
+    new Product('Pallina Giocattolo', 5.99, './img/pallina-per-cani.jpg', $categoryCats, ItemType::Toy),
+    new Product('Cuccia per Gatti', 45.50, './img/cuccia-per-gatti.jpg', $categoryCats, ItemType::Kennel),
+    new Product('Guinzaglio per Cani', 12.99, './img/guinzaglio-per-cani.jpg', $categoryDogs, ItemType::Toy),
+    new Product('Tiragraffi per Gatti', 30.00, './img/Tiragraffi-per-gatti.jpg', $categoryCats, ItemType::Kennel),
+    new Product('Snack per Gatti', 4.99, './img/snack-per-gatti.jpg', $categoryCats, ItemType::Food),
+    new Product('Ciotola per Cani', 8.50, './img/ciotole-per-cani.jpg', $categoryDogs, ItemType::Kennel),
+    new Product('Osso di Gomma', 6.00, './img/osso-di-gomma.jpg', $categoryDogs, ItemType::Toy),
+    new Product('Gioco per Gatti con Piume', 7.50, './img/gioco-gatti.jpg', $categoryCats, ItemType::Toy),
+    new Product('Cuccia per Cani', 50.99, './img/cuccia-per-cani.jpg', $categoryDogs, ItemType::Kennel),
+    new Product('Spazzola per Gatti', 9.99, './img/spazzola-per-gatti.png', $categoryCats, ItemType::WellBeing),
+    new Product('Shampoo per Cani', 14.99, './img/shampoo-per-cani.jpg', $categoryDogs, ItemType::WellBeing),
+];
 
 ?>
 
